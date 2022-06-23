95fc6a3cb70d648dad75eaf824baa12eb9048e47
Surajit Karuri <surajitkaruri@gmail.com>
2022-06-23 18:47:18 +0530

    signin, signup local bootstrap dist css & js

--- src/main/java/link/karurisuro/peopledirect/controllers/contactController.java
+++ src/main/java/link/karurisuro/peopledirect/controllers/contactController.java
@@ -1,29 +1,50 @@
 package link.karurisuro.peopledirect.controllers;
 
+import link.karurisuro.peopledirect.entities.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@Slf4j
 public class contactController {
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("title", "People Direct | Home");
         model.addAttribute("app", "Welcome to PeopleDirect");
-        model.addAttribute("page", "Home");
-
         return "index";
     }
 
     @GetMapping("/about")
     public String about(Model model) {
-        model.addAttribute("title", "People Direct | About");
-        model.addAttribute("app", "Welcome to PeopleDirect");
-        model.addAttribute("page", "About");
-
+        model.addAttribute("title", " About");
         return "about";
+    }
+
+    @GetMapping("/signup")
+    public String loadSignUp(Model model) {
+        model.addAttribute("title", "Sign Up");
+        model.addAttribute("user", new User());
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String addSignUp(@ModelAttribute User user) {
+        log.debug("model attribute:\n {}", user);
+        return "redirect:/signin";
+    }
+
+    @GetMapping("/signin")
+    public String loadSignIn(Model model) {
+        model.addAttribute("title", "Sign Up");
+        model.addAttribute("user", new User());
+        return "signin";
+    }
+
+    @PostMapping("/signin")
+    public String addSignIn(@ModelAttribute User user) {
+        log.debug("model attribute:\n {}", user);
+        return "redirect:/signin";
     }
 }
